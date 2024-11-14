8bbd0c5419725637ec5a86d736e0d2e91f48f4f9
Maxim <maxtalab@gmail.com>
2024-11-14 17:19:14 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,7 @@
+
+URL_SERVICE = "https://f287295e-dd7a-4f4a-8b83-47720f3caefa.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/" #путь до логов
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv" #путь до таблицы базы данных
+CREATE_USER_PATH = "/api/v1/users/"  #путь к методу создания пользователя
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/" #путь к методу поиска  наборов
